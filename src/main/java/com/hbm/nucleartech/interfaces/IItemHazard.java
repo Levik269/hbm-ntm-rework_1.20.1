53e5fbb72b0d9ada7b8abb67ef3890f7a56e737a
hlnikniky <orekhovs09@gmail.com>
2026-03-24 01:18:04 +0700

    Add Checkers and getters

--- src/main/java/com/hbm/nucleartech/interfaces/IItemHazard.java
+++ src/main/java/com/hbm/nucleartech/interfaces/IItemHazard.java
@@ -7,7 +7,29 @@ import net.minecraft.world.level.block.Block;
 public interface IItemHazard {
 
     public ItemHazardModule getModule();
-
+    // --------- Getters ---------
+    public default double getRadiationLevel() {
+        return this.getModule().getRadiationLevel();
+    }
+    public default double getDigammaLevel() {
+        return this.getModule().getDigammaLevel();
+    }
+    public default int getFireLevel() {
+        return this.getModule().getFireLevel();
+    }
+    public default int getCryogenicLevel() {
+        return this.getModule().getCryogenicLevel();
+    }
+    public default int getToxicLevel() {
+        return this.getModule().getToxicLevel();
+    }
+    public default int getAsbestosLevel() {
+        return this.getModule().getAsbestosLevel();
+    }
+    public default int getCoalLevel() {
+        return this.getModule().getCoalLevel();
+    }
+    // --------- Adders ---------
     public default IItemHazard addRadiation(double radiation) {
 
         this.getModule().addRadiation(radiation);
@@ -67,11 +89,52 @@ public interface IItemHazard {
         this.getModule().addExplosive(bang);
         return this;
     }
-
+    // --------- Checkers ---------
     public default boolean isRadioactive() {
 
         return this.getModule().isRadioactive();
     }
+    public default boolean isBlinding() {
+
+        return this.getModule().isBlinding();
+    }
+    public default boolean isHydroReactive() {
+
+        return this.getModule().isHydroReactive();
+    }
+    public default boolean isExplosive() {
+
+        return this.getModule().isExplosive();
+    }
+    public default boolean isToxic() {
+
+        return this.getModule().isToxic();
+    }
+    public default boolean isFireHazard() {
+
+        return this.getModule().isFireHazard();
+    }
+    public default boolean isCryogenic() {
+
+        return this.getModule().isCryogenic();
+    }
+    public default boolean isDigamma() {
+
+        return this.getModule().isDigamma();
+    }
+    public default boolean isCoal() {
+
+        return this.getModule().isCoal();
+    }
+    public default boolean isAsbestos() {
+
+        return this.getModule().isAsbestos();
+    }
+    public default boolean isHazardous() {
+
+        return this.getModule().isHazardous();
+    }
+    // --------- Converters ---------
 
     public default Item toItem() {
 
