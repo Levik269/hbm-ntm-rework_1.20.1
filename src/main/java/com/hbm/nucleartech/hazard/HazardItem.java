2ead2dc245e085e741adae4299593131742a291e
hlnikniky <orekhovs09@gmail.com>
2026-03-25 21:04:16 +0700

    Update method to add hazards from overloads to builder

--- src/main/java/com/hbm/nucleartech/hazard/HazardItem.java
+++ src/main/java/com/hbm/nucleartech/hazard/HazardItem.java
@@ -16,153 +16,115 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 
 public class HazardItem extends Item implements IItemHazard {
+/*Example code
+* public static final RegistryObject<Item> EXAMPLE_ITEM =
+    ITEMS.register("example_item",
+        () -> new HazardItem.Builder(new Item.Properties())
+                .asbestos(1)
+                .build()
+    );
+* */
+    private final ItemHazardModule module;
 
-    ItemHazardModule module;
-
-    public HazardItem(double radiation, Properties pProperties) {
-        super(pProperties);
-        this.module = new ItemHazardModule();
-        this.module.addRadiation(radiation);
-    }
-
-    public HazardItem(double radiation, double digamma, Properties pProperties) {
-        super(pProperties);
-        this.module = new ItemHazardModule();
-        this.module.addRadiation(radiation);
-        this.module.addDigamma(digamma);
-    }
-
-    public HazardItem(double radiation, double digamma, int fire, Properties pProperties) {
-        super(pProperties);
-        this.module = new ItemHazardModule();
-        this.module.addRadiation(radiation);
-        this.module.addDigamma(digamma);
-        this.module.addFire(fire);
-    }
-
-    public HazardItem(double radiation, double digamma, int fire, int cryogenic, Properties pProperties) {
-        super(pProperties);
-        this.module = new ItemHazardModule();
-        this.module.addRadiation(radiation);
-        this.module.addDigamma(digamma);
-        this.module.addFire(fire);
-        this.module.addCryogenic(cryogenic);
-    }
-
-    public HazardItem(double radiation, double digamma, int fire, int cryogenic, int toxiclvl, Properties pProperties) {
-        super(pProperties);
-        this.module = new ItemHazardModule();
-        this.module.addRadiation(radiation);
-        this.module.addDigamma(digamma);
-        this.module.addFire(fire);
-        this.module.addCryogenic(cryogenic);
-        this.module.addToxic(toxiclvl);
-    }
-
-    public HazardItem(double radiation, double digamma, int fire, int cryogenic, int toxiclvl, int asbestos, Properties pProperties) {
-        super(pProperties);
-        this.module = new ItemHazardModule();
-        this.module.addRadiation(radiation);
-        this.module.addDigamma(digamma);
-        this.module.addFire(fire);
-        this.module.addCryogenic(cryogenic);
-        this.module.addToxic(toxiclvl);
-        this.module.addAsbestos(asbestos);
-    }
-
-    public HazardItem(double radiation, double digamma, int fire, int cryogenic, int toxiclvl, int asbestos, int coal, Properties pProperties) {
-        super(pProperties);
-        this.module = new ItemHazardModule();
-        this.module.addRadiation(radiation);
-        this.module.addDigamma(digamma);
-        this.module.addFire(fire);
-        this.module.addCryogenic(cryogenic);
-        this.module.addToxic(toxiclvl);
-        this.module.addAsbestos(asbestos);
-        this.module.addCoal(coal);
-    }
-
-    public HazardItem(double radiation, double digamma, int fire, int cryogenic, int toxiclvl, int asbestos, int coal, boolean blinding, Properties pProperties) {
-        super(pProperties);
-        this.module = new ItemHazardModule();
-        this.module.addRadiation(radiation);
-        this.module.addDigamma(digamma);
-        this.module.addFire(fire);
-        this.module.addCryogenic(cryogenic);
-        this.module.addToxic(toxiclvl);
-        this.module.addAsbestos(asbestos);
-        this.module.addCoal(coal);
-        if(blinding)
-            this.module.addBlinding();
-    }
-
-    public HazardItem(double radiation, double digamma, int fire, int cryogenic, int toxiclvl, int asbestos, int coal, boolean blinding, boolean hydroReactive, Properties pProperties) {
-        super(pProperties);
-        this.module = new ItemHazardModule();
-        this.module.addRadiation(radiation);
-        this.module.addDigamma(digamma);
-        this.module.addFire(fire);
-        this.module.addCryogenic(cryogenic);
-        this.module.addToxic(toxiclvl);
-        this.module.addAsbestos(asbestos);
-        this.module.addCoal(coal);
-        if(blinding)
-            this.module.addBlinding();
-        if(hydroReactive)
-            this.module.addHydroReactivity();
-    }
-
-    public HazardItem(double radiation, double digamma, int fire, int cryogenic, int toxiclvl, int asbestos, int coal, boolean blinding, boolean hydroReactive, float explosive, Properties pProperties) {
-        super(pProperties);
-        this.module = new ItemHazardModule();
-        this.module.addRadiation(radiation);
-        this.module.addDigamma(digamma);
-        this.module.addFire(fire);
-        this.module.addCryogenic(cryogenic);
-        this.module.addToxic(toxiclvl);
-        this.module.addAsbestos(asbestos);
-        this.module.addCoal(coal);
-        if(blinding)
-            this.module.addBlinding();
-        if(hydroReactive)
-            this.module.addHydroReactivity();
-        this.module.addExplosive(explosive);
+    private HazardItem(Builder builder) {
+        super(builder.properties);
+        this.module = builder.module;
     }
 
     @Override
     public ItemHazardModule getModule() {
-
         return this.module;
     }
-
 
     @Override
     public void inventoryTick(ItemStack pStack, Level pLevel, Entity pEntity, int pSlotId, boolean pIsSelected) {
         super.inventoryTick(pStack, pLevel, pEntity, pSlotId, pIsSelected);
 
-        if(!pLevel.isClientSide && pEntity instanceof LivingEntity)
+        if (!pLevel.isClientSide && pEntity instanceof LivingEntity living) {
             this.module.applyEffects(
-                    (LivingEntity) pEntity,
+                    living,
                     pStack.getCount(),
                     pSlotId,
                     pIsSelected,
-                    ((LivingEntity)pEntity).getMainHandItem() == pStack ? InteractionHand.MAIN_HAND : InteractionHand.OFF_HAND
+                    living.getMainHandItem() == pStack ? InteractionHand.MAIN_HAND : InteractionHand.OFF_HAND
             );
+        }
     }
 
     @Override
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
-
         this.module.addInformation(pStack, pTooltipComponents, pIsAdvanced);
         super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
     @Override
     public boolean onEntityItemUpdate(ItemStack stack, ItemEntity entity) {
-
         boolean m = this.module.onEntityItemUpdate(entity);
         boolean i = super.onEntityItemUpdate(stack, entity);
-
         return m || i;
+    }
+
+    public static class Builder {
+
+        private final Item.Properties properties;
+        private final ItemHazardModule module = new ItemHazardModule();
+
+        public Builder(Item.Properties properties) {
+            this.properties = properties;
+        }
+
+        public Builder radiation(double value) {
+            module.addRadiation(value);
+            return this;
+        }
+
+        public Builder digamma(double value) {
+            module.addDigamma(value);
+            return this;
+        }
+
+        public Builder fire(int value) {
+            module.addFire(value);
+            return this;
+        }
+
+        public Builder cryogenic(int value) {
+            module.addCryogenic(value);
+            return this;
+        }
+
+        public Builder toxic(int value) {
+            module.addToxic(value);
+            return this;
+        }
+
+        public Builder asbestos(int value) {
+            module.addAsbestos(value);
+            return this;
+        }
+
+        public Builder coal(int value) {
+            module.addCoal(value);
+            return this;
+        }
+
+        public Builder blinding() {
+            module.addBlinding();
+            return this;
+        }
+
+        public Builder hydroReactive() {
+            module.addHydroReactivity();
+            return this;
+        }
+
+        public Builder explosive(float value) {
+            module.addExplosive(value);
+            return this;
+        }
+
+        public HazardItem build() {
+            return new HazardItem(this);
+        }
     }
 }
